5c9f6229dbd0574444e1e2cd18d7c286a9beda30
PepinhoJp <pepinho.jp@gmail.com>
2022-09-14 09:20:15 -0300

    Update admin.py

--- psiu/admin.py
+++ psiu/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Usuario, Carona, Estudos, Extra, Ligas
+
+admin.site.register(Usuario)
+admin.site.register(Carona)
+admin.site.register(Estudos)
+admin.site.register(Extra)
+admin.site.register(Ligas)
