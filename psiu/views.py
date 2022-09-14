14182fc9ece008e9726c3eacb11a4d503feffcf7
PepinhoJp <pepinho.jp@gmail.com>
2022-09-14 10:42:02 -0300

    Update views.py

--- psiu/views.py
+++ psiu/views.py
@@ -20,14 +20,13 @@ def ligas_academicas(request):
 def conhecer_pessoas(request):
     return render(request, 'base.html',{'title':'Conhecer Pessoas'})
 def teste(request):
-        if request.method == 'GET':
-        context = {'title': 'User Form Page'}
-        template = 'films/user_form.html'
+    if request.method == 'GET':
+    context = {'title': 'User Form Page'}
+    template = 'films/user_form.html'
 
-        return render(request,
-                      template,
-                      context)
-        
+    return render(request,
+                  template,
+                  context)
     elif request.method == 'POST':
         username = request.POST.get('username')
         country = request.POST.get('country')
