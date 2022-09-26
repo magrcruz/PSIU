7a35a7f38b1833a6ee613afd9acea494e652103a
PepinhoJp <pepinho.jp@gmail.com>
2022-09-26 10:28:46 -0300

    Adicionar checagem de login

--- psiu/views.py
+++ psiu/views.py
@@ -24,98 +24,135 @@ def main(request):
         return redirect(reverse('psiu:login'))
 
 def user_info(request):
-    return render(request, 'psiu/user_info.html')
+    if request.user.is_authenticated:
+        return render(request, 'psiu/user_info.html')
+    else:
+        return redirect(reverse('psiu:login'))
 
 def carona(request):
-    carona_list = Carona.objects.all().values() #to update with filters
-    # to add nome of user
-    for carona in carona_list:
-        if 'criador_id' in carona_list and carona_list['criador_id']!="NULL":
-            name = Perfil.objects.get(pk=carona_list['criador_id'])
-            if name:
-                carona['nomeUser'] = name
-            continue
-        carona['nomeUser'] = "User not found"
+    if request.user.is_authenticated:
+        carona_list = Carona.objects.all().values() #to update with filters
+        # to add nome of user
+        for carona in carona_list:
+            if 'criador_id' in carona_list and carona_list['criador_id']!="NULL":
+                name = Perfil.objects.get(pk=carona_list['criador_id'])
+                if name:
+                    carona['nomeUser'] = name
+                continue
+            carona['nomeUser'] = "User not found"
 
-    return render(request, 'psiu/carona.html',{'title':'Carona', 'carona_list':carona_list})
+        return render(request, 'psiu/carona.html',{'title':'Carona', 'carona_list':carona_list})
+    else:
+        return redirect(reverse('psiu:login'))
 
 def criar_carona(request):
-    print(request)
-    if request.method == "GET":
-        carona_temp = None
-        return render(request, 'psiu/criar_carona.html', {'carona_temp':carona_temp})
+    if request.user.is_authenticated:
+        print(request)
+        if request.method == "GET":
+            carona_temp = None
+            return render(request, 'psiu/criar_carona.html', {'carona_temp':carona_temp})
 
-    elif request.method == "POST":
-        carona = Carona()
+        elif request.method == "POST":
+            carona = Carona()
 
-        # there is a better way to do this with forms.py
-        form = request.POST.dict()
-        fields = carona.get_readonly_fields(request)
-        content = {} 
-        for field in fields:
-            if field in form:
-                content[field] = form.get(field)
+            # there is a better way to do this with forms.py
+            form = request.POST.dict()
+            fields = carona.get_readonly_fields(request)
+            content = {} 
+            for field in fields:
+                if field in form:
+                    content[field] = form.get(field)
 
-        #content['criador_id'] = 
-        print(request.user)
-        #print( content['dataHora'])
-        #content['dataHora'] = datetime.strptime(content['dataHora'], '%Y-%m-%dT%H:%M')
-        #print( content['dataHora'])
+            #content['criador_id'] = 
+            print(request.user)
+            #print( content['dataHora'])
+            #content['dataHora'] = datetime.strptime(content['dataHora'], '%Y-%m-%dT%H:%M')
+            #print( content['dataHora'])
 
-        carona = Carona(**content)
-        #carona.add_to_class('dataHora', content['dataHora'])
-        carona.save()
+            carona = Carona(**content)
+            #carona.add_to_class('dataHora', content['dataHora'])
+            carona.save()
 
-        return redirect(reverse('psiu:carona'))
+            return redirect(reverse('psiu:carona'))
+    else:
+        return redirect(reverse('psiu:login'))
+
 
 def view_carona(request):
-    return render(request, 'psiu/info_carona.html',{})
+    if request.user.is_authenticated:
+        return render(request, 'psiu/info_carona.html',{})
+    else:
+        return redirect(reverse('psiu:login'))
+
 
 def grupo_estudos(request):
-    return render(request, 'base.html',{'title':'Grupo de estudos'})
+    if request.user.is_authenticated:
+        return render(request, 'base.html',{'title':'Grupo de estudos'})
+    else:
+        return redirect(reverse('psiu:login'))
+
+    
 def extracurriculares(request):
-    return render(request, 'base.html',{'title':'Actividades extracurriculares'})
+    if request.user.is_authenticated:
+        return render(request, 'base.html',{'title':'Actividades extracurriculares'})
+    else:
+        return redirect(reverse('psiu:login'))
+
+
 def ligas_academicas(request):
-    return render(request, 'base.html',{'title':'Ligas Academicas'})
+    if request.user.is_authenticated:
+        return render(request, 'base.html',{'title':'Ligas Academicas'})
+    else:
+        return redirect(reverse('psiu:login'))
+
+
 def conhecer_pessoas(request):
-    return render(request, 'base.html',{'title':'Conhecer Pessoas'})
+    if request.user.is_authenticated:
+        return render(request, 'base.html',{'title':'Conhecer Pessoas'})
+    else:
+        return redirect(reverse('psiu:login'))
 
 def register_request(request):
-    if request.method == "POST":
-        form = NewUserForm(request.POST)
-        profile_form = PerfilForm(request.POST, request.FILES)
-        if form.is_valid() and profile_form.is_valid():
-            user = form.save()
-            login(request, user)
-            profile = profile_form.save(commit=False)
-            profile.user = request.user
-            profile.save()
-            messages.success(request, "Registration successful." )
-            return redirect("home")
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            form = NewUserForm(request.POST)
+            profile_form = PerfilForm(request.POST, request.FILES)
+            if form.is_valid() and profile_form.is_valid():
+                user = form.save()
+                login(request, user)
+                profile = profile_form.save(commit=False)
+                profile.user = request.user
+                profile.save()
+                messages.success(request, "Registration successful." )
+                return redirect("home")
 
-        messages.error(request, "Unsuccessful registration. Invalid information.")
-    form = NewUserForm()
-    profile_form = PerfilForm()
-    return render (request, "psiu/criar.html", {"criar_form":form, 'profile_form': profile_form})
-
+            messages.error(request, "Unsuccessful registration. Invalid information.")
+        form = NewUserForm()
+        profile_form = PerfilForm()
+        return render (request, "psiu/criar.html", {"criar_form":form, 'profile_form': profile_form})
+    else:
+        return redirect(reverse('psiu:login'))
 
 def login_request(request):
-	if request.method == "POST":
-		form = AuthenticationForm(request, data=request.POST)
-		if form.is_valid():
-			username = form.cleaned_data.get('username')
-			password = form.cleaned_data.get('password')
-			user = authenticate(username=username, password=password)
-			if user is not None:
-				login(request, user)
-				messages.info(request, f"You are now logged in as {username}.")
-				return redirect("home")
-			else:
-				messages.error(request,"Invalid username or password.")
-		else:
-			messages.error(request,"Invalid username or password.")
-	form = AuthenticationForm()
-	return render(request, "psiu/login.html", {"login_form":form})
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            form = AuthenticationForm(request, data=request.POST)
+            if form.is_valid():
+                username = form.cleaned_data.get('username')
+                password = form.cleaned_data.get('password')
+                user = authenticate(username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    messages.info(request, f"You are now logged in as {username}.")
+                    return redirect("home")
+                else:
+                    messages.error(request,"Invalid username or password.")
+            else:
+                messages.error(request,"Invalid username or password.")
+        form = AuthenticationForm()
+        return render(request, "psiu/login.html", {"login_form":form})
+    else:
+        return redirect(reverse('psiu:login'))
 
 
 def logout_request(request):
