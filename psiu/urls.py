3fb993accba4c0eac12c0275a444b9013378a2bb
Magr Cruz <maria.cruz@ucsp.edu.pe>
2022-11-21 01:04:20 -0300

    user info

--- psiu/urls.py
+++ psiu/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'psiu'
 urlpatterns = [
     path('', views.home, name='main'),
-    path('user_info/', views.user_info, name='user_info'),
+    path('user_info/<id>', views.user_info, name='user_info'),
     path('carona/', views.carona, name='carona'),
     path('grupo_estudos/', views.grupo_estudos, name='grupo_estudos'),
     path('extracurriculares/', views.extracurriculares, name='extracurriculares'),
@@ -42,7 +42,6 @@ urlpatterns = [
     #views
     path('view_carona/<id>', views.view_carona, name='view_carona'),
     path('info_estudos/<id>', views.info_estudos, name='info_estudos'),
-    path('info_perfil/<id>', views.info_perfil, name='info_perfil'),
     path('info_extracurricular/<id>', views.info_extracurricular, name='info_extracurricular'),
     path('info_ligas_nao_oficiais/<id>', views.info_ligas_nao_oficiais, name='info_ligas_nao_oficiais'),
 ]
